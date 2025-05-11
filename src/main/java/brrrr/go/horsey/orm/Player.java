49641332ce79aef6703297956c20a799b63e800f
heofthetea <heofthetea@gmail.com>
2025-05-11 10:28:16 +0200

    remove user email field

--- src/main/java/brrrr/go/horsey/orm/Player.java
+++ src/main/java/brrrr/go/horsey/orm/Player.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
  * ORM Model for a user/player.
  * The terminology is a bit mixed up sadly, all originating from the fact that it has to be called differently in the database.
  * Is acceptable only because I'm the only one working on this and eh, it works
+ * Edit: This really only is a mapper entity to map users from keycloak to stuff in the main database
  */
 @Entity
 @Table(name = "player") // cannot name a table 'user' in postgres
@@ -15,28 +16,15 @@ public class Player {
     @Column(name = "username")
     private String username;
 
-    @Column(nullable = false)
-    private String email;
-
-    public Player(String username, String password, String email) {
+    public Player(String username) {
         this.username = username;
-        this.email = email;
     }
 
     // default constructor is required by JPA somehow
     public Player() {
         this.username = "";
-        this.email = "";
     }
 
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getUsername() {
         return username;
